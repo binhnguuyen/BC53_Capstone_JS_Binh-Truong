f0d78baaf42a82546a5515b0aaa88c9c0ab21389
binhnguyen <leechiupin@gmail.com>
2023-09-23 14:12:55 +0900

    Thêm dropdown cho người dùng lọc hiển thị

--- customer/controller/Controller.js
+++ customer/controller/Controller.js
@@ -6,7 +6,7 @@ function renderProductsList(productsList) {
     var contentTr = `
             <div class="my-4 col-12 col-lg-4 col-sm-6">
                 <div class="card animate__animated animate__zoomIn">
-                    <img src="${product.img}" class="card-img-top" alt="" width="200" height="300">
+                    <img src="${product.img}" class="card-img-top" alt="" width="200" height="350">
                     <div>
                         <h5>${product.name}</h5>
                         <p><span>Price </span>${product.price}<span> $</span></p>
@@ -15,26 +15,6 @@ function renderProductsList(productsList) {
                 </div>
             </div> 
         `;
-    /*`
-            <tr>
-                <td>${product.id}</td>
-                <td>${product.name}</td>
-                <td>${product.price}</td>
-                <td>${product.screen}</td>
-                <td>${product.backCamera}</td>
-                <td>${product.frontCamera}</td>
-                <td>
-                    <img src="${product.img}" alt="" width="200" height="200" />
-                </td>
-                <td>${product.desc}</td>
-                <td>${product.type}</td>
-                <td>
-                    <button class="btn btn-warning" onclick="editProduct(${product.id})">Sửa</button>
-                    <button class="btn btn-danger" onclick="delProduct(${product.id})">Xoá</button>
-                </td>
-            </tr>
-        `
-        */
     content += contentTr;
   }
 
@@ -81,67 +61,11 @@ function offLoading() {
   document.querySelector("#spinner").style.display = "none";
 }
 
-function validationSP(value) {
-  var tbmaSP = getEle("#tbmaSP");
-  var nameProduct = getEle("#tbTenSP");
-  var priceProduct = getEle("#tbGiaSP");
-  var screenProduct = getEle("#tbscreenSP");
-  var backCameraProduct = getEle("#tbbackCameraSP");
-  var frontCameraProduct = getEle("#tbfrontCameraSP");
-  var imageProduct = getEle("#tbHinhSP");
-  var describeProduct = getEle("#tbdescSP");
-  var typeProduct = getEle("#tbloaiSP");
 
-  var messBlankName = "Tên sản phẩm không đc để trống";
-  var messBlankPrice = "Giá sản phẩm không đc để trống";
-  var messMinMaxPrice = "Giá phải là số trong 50000-100000000";
-  var messCheckNnum = "Giá sản phẩm phải là số";
-  var messBlankScreen = "Màn hình sản phẩm không đc để trống";
-  var messBlankBKCam = "Camera sau không đc để trống";
-  var messBlankFRCam = "Camera trước không đc để trống";
 
-  var messBlankImg = "Link hình ảnh sản phẩm không đc để trống";
-  var messBlankDesc = "Mô tả sản phẩm không đc để trống";
-  var messCheckType = "Hãng sản xuất không hợp lệ";
 
-  // kiểm tra tên sp có bỏ trống hay ko
-  var valid = kiemTraRong(value.name, nameProduct, messBlankName);
 
-  // kiểm tra giá sp có bỏ trống, có phải là số và nằm trong đúng khung giá hay ko
-  valid &=
-    kiemTraRong(value.price, priceProduct, messBlankPrice) &&
-    kiemTraSo(value.price, priceProduct, messCheckNnum) &&
-    kiemTraLonNho(value.price, priceProduct, 50000, 100000000, messMinMaxPrice);
 
-  // kiểm tra màn hình sp có bỏ trống hay ko
-  valid &= kiemTraRong(value.screen, screenProduct, messBlankScreen);
 
-  // kiểm tra camera sau sp có bỏ trống hay ko
-  valid &= kiemTraRong(value.backCamera, backCameraProduct, messBlankBKCam);
 
-  // kiểm tra camera trước sp có bỏ trống hay ko
-  valid &= kiemTraRong(value.frontCamera, frontCameraProduct, messBlankFRCam);
 
-  // kiểm tra hình ảnh sp có bỏ trống hay ko
-  valid &= kiemTraRong(value.img, imageProduct, messBlankImg);
-
-  // kiểm tra miêu tả sp có bỏ trống hay ko
-  valid &= kiemTraRong(value.desc, describeProduct, messBlankDesc);
-
-  // kiểm tra loại sp có bỏ trống hay ko
-  var hangSX = ["apple", "samsung"];
-  valid &= kiemTraTrung(value.type, hangSX, typeProduct, messCheckType);
-
-  if (valid) {
-    return valid;
-  } else {
-    nameProduct.style.display = "block";
-    priceProduct.style.display = "block";
-    screenProduct.style.display = "block";
-    backCameraProduct.style.display = "block";
-    frontCameraProduct.style.display = "block";
-    imageProduct.style.display = "block";
-    describeProduct.style.display = "block";
-    typeProduct.style.display = "block";
-  }
-}
--- customer/customer.js
+++ customer/customer.js
@@ -1,37 +1,80 @@
 function fetchProductsList() {
-    // trước khi load axios
-    onLoading();
-  
-    // gọi axios ra
-    // axios({
-    //     url: "https://6500589318c34dee0cd4bff0.mockapi.io/products",
-    //     method: "GET",
-    // })
-    //     // ở đây then chỉ cần data thôi nên chỉ cần res.data
-    //     .then(function (res) {
-    //         console.log('res: ', res.data);
-    //         renderProductList(res.data);
-    //         // sau khi load đc data từ backend rồi
-    //         offLoading();
-    //     })
-    //     .catch(function (err) {
-    //         console.log('err: ', err);
-    //     });
-  
-    // định nghĩa bên ProductsServ
-    getProductList()
-      // thành công thì in ra sp
-      .then(function (res) {
-        // console.log("res", res.data);
-        renderProductsList(res.data);
-        offLoading();
-      })
-      // thất bại thì báo lỗi, hoặc in ra thông báo tuỳ mình
-      .catch(function (err) {
-        offLoading();
-        console.log("err", err);
-      });
-    // cách 2: productServ().getProductList();
-  }
-  
-  fetchProductsList();
+  // trước khi load axios
+  onLoading();
+
+  // gọi axios ra
+  // axios({
+  //     url: "https://6500589318c34dee0cd4bff0.mockapi.io/products",
+  //     method: "GET",
+  // })
+  //     // ở đây then chỉ cần data thôi nên chỉ cần res.data
+  //     .then(function (res) {
+  //         console.log('res: ', res.data);
+  //         renderProductList(res.data);
+  //         // sau khi load đc data từ backend rồi
+  //         offLoading();
+  //     })
+  //     .catch(function (err) {
+  //         console.log('err: ', err);
+  //     });
+
+  // định nghĩa bên ProductsServ
+  getProductList()
+    // thành công thì in ra sp
+    .then(function (res) {
+      // console.log("res", res.data);
+      renderProductsList(res.data);
+      offLoading();
+    })
+    // thất bại thì báo lỗi, hoặc in ra thông báo tuỳ mình
+    .catch(function (err) {
+      offLoading();
+      console.log("err", err);
+    });
+  // cách 2: productServ().getProductList();
+}
+
+fetchProductsList();
+
+function filSP() {
+  var filterSP = document.querySelector("#filterSP").value;
+  console.log("filterSP: ", filterSP);
+  var tbFilterSP = document.querySelector("#tbFilterSP");
+  var filterResult = [];
+  var all = "All";
+
+  getProductList()
+    // thành công thì in ra sp
+    .then(function (res) {
+      var sp = res.data;
+
+      if (filterSP === all) {
+        renderProductsList(sp);
+      } 
+      else {
+        for (var i = 0; i < sp.length; i++) {
+          if (filterSP === sp[i].type) {
+            filterResult.push(sp[i]);
+          }
+        }
+        renderProductsList(filterResult);
+      }
+      if (filterSP != all) {
+        if (filterResult.length == 0) {
+          tbFilterSP.classList.remove("d-none");
+          tbFilterSP.classList.add("d-block");
+        } 
+        else {
+          tbFilterSP.classList.remove("d-block");
+          tbFilterSP.classList.add("d-none");
+        }
+      }
+
+      offLoading();
+    })
+    // thất bại thì báo lỗi, hoặc in ra thông báo tuỳ mình
+    .catch(function (err) {
+      offLoading();
+      console.log("err", err);
+    });
+}
