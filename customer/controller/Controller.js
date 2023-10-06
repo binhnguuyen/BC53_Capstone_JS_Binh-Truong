61fa21394a5d5419316ad85b67b4b04dd9245dac
QuangTruong <nqtruong2210@gmail.com>
2023-10-06 17:40:48 +0700

    update dark-theme

--- customer/controller/Controller.js
+++ customer/controller/Controller.js
@@ -219,3 +219,14 @@ if (dataJson !== null) {
     // console.log('dsspInCart: ', dsspInCart.product);
     renderProductsToCart(dsspInCart.product);
 }
+
+// Check darkMode
+var checkbox = document.querySelector('input[name="darkMode"]');
+checkbox.addEventListener('change',function(){
+    if(this.checked){
+        document.querySelector('body').classList.add("darkMode");
+    }else{
+        document.querySelector('body').classList.remove("darkMode");
+    }
+})
+
